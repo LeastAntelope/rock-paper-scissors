d322d8c121c834fc9cc4b2ea39a5db46b49fd9f0
JJ <jjbeen24@gmail.com>
2022-02-28 00:27:57 -0500

    Finished code. Lack of commits due to full time job.

--- js/game.js
+++ js/game.js
@@ -1,43 +1,109 @@
-//Rock-Paper-Scissor
-//1. Make the cpu choose a random choice.
-    //a. Generate a random number using Math.random().
-    //b. Said number will be a decimal between 0 and 1. we will multiply that by 100
-       //to get decimal between 0 and 99.
-    //c. We will then need to round that number down to make it whole.
-    //d. With our random whole number between 0 and 99 (now x),
-       //we can make our conditions.
-    //e. If x <= 33 choose rock.
-    //f. If x >= 34 && x <= 66 choose paper.
-    //g. If x >= 67 choose scissors.
-    //h. Put this choice into a variable named "computerSelection".
+let computerScore = 0;
+let playerScore = 0;
+let computerSelection;
+let playerSelection;
+let roundResults;
+gamePlay();
 
-//2. Create a function to determine the playerSelection variable.
-    //a. Upon opening the page display a prompt. The prompt should say
-       //"rock, paper, or scissors?".
-    //b. Evaluate the input against a regular expression, as follows.
-       //check if input = any variation of rock, paper, or scissors.
-       //if not then return, "sorry, that's not a valid selection."
-       //return only the first match, to ensure no errors.
-    //c. standardize the format of this choice to the code.
-    //d. place this choice in playerSelection
+function gamePlay() {
+   let playcount = 0;
+      while (playcount < 5) {
+      computerPlay();
+      selectprompt();
+      playRound(playerSelection, computerSelection);
+      if (roundResults == "player") {
+         playerScore = ++playerScore;
+         playcount = ++playcount;
+      }
+      else if (roundResults == "cpu") {
+         computerScore = ++computerScore;
+         playcount = ++playcount;
+      }
+   }
+   if (playerScore > computerScore) {
+      alert(`congratulations! you won the game!
+      Score: ${playerScore} - ${computerScore}`);
+      let winrestart = prompt("do you want to try again?", "yes");
+      if (winrestart == "yes") {
+         computerScore = 0;
+         playerScore = 0;
+         gamePlay();
+      }
+   }
+   else {
+      let restartselect = prompt(`Sorry. You lost. Do you want to try again?
+      Score: ${computerScore} - ${playerScore}`, "yes");
+      if (restartselect == "yes") {
+         computerScore = 0;
+         playerScore = 0;
+         gamePlay();
+      }
+   }
+}
 
-//3. Create a function to play a single round using (computerSelection,playerSelection).
-    //a. Take both selections and see if they are equal. If so return "tie".
-       //If not proceed.
-    //b. 3 conditions here.
-       //If playerSelection || computerSelection = "rock" &&
-       //playerSelection || computerSelection = "paper". Then...
-       //If playerSelection = "paper". Display
-       //"Congratulations! paper covers rock!".
-       //else display "Try again. paper covers rock."
-    //c. Iterate this setup over the other 2 varitaions.
-    //d. Put the winner into a variable and return the variable.
-       
-//4. This game will be played in sets of 5. With the winner winning after 3.
-    //a. Create a for loop that iterates to 5 games.
-    //b. At the start of this loop check the score. If either player has won three games,
-       //exit the loop.
-    //c. Upon exit display a prompt stating the winner,
-       //and ask the user to play again with a yes or no prompt.
-    //d. Display the score, and current game's winner,
-       //in the next playerSelection prompt. Or in the exit prompt
+function computerPlay() {
+    const playnumber = Math.floor(Math.random() * 100);
+    if (playnumber <= 32) {
+      return computerSelection = "rock";
+    } 
+    else if (playnumber >= 33 && playnumber <= 65) {
+      return computerSelection = "paper";
+    }
+    else {
+      return computerSelection = "scissors";
+    }
+}
+
+function selectprompt() {
+   let selection = prompt("Rock, Paper, or Scissors?", "rock");
+   if (selection == null) {
+      alert("To quit close the page.");
+      selection = "rock"
+      selectprompt();
+   }
+   selection = selection.toLowerCase();
+   if (selection == "rock" || selection == "paper" || selection == "scissors") {
+      return playerSelection = selection;
+   } 
+   else {
+      alert("Sorry. That's not a valid selection.");
+      selectprompt();
+   }
+}
+
+function playRound(playerSelection, computerSelection) {
+     if (playerSelection == computerSelection) {
+        alert("Tie Round.");
+        return roundResults = "tie";
+     }
+     else if ((playerSelection == "rock" || computerSelection == "rock") &&
+       (playerSelection == "paper" || computerSelection == "paper")) {
+         if (playerSelection == "paper") {
+            alert("Congratulations! Paper covers Rock!");
+            return roundResults = "player";
+         }
+         else {
+            alert("Try again. Paper covers Rock.");
+            return roundResults = "cpu"
+         } 
+      }
+      else if ((playerSelection == "rock" || computerSelection == "rock") &&
+       (playerSelection == "scissors"|| computerSelection == "scissors")) {
+         if (playerSelection == "rock") {
+            alert("Congratulations! Rock smashes Scissors!");
+            return roundResults = "player";
+         }
+         else {
+            alert("Try again. Rock smashes Scissors.");
+            return roundResults = "cpu"
+         } 
+      }
+      else if (playerSelection == "scissors") {
+            alert("Congratulations! Scissors cuts Paper!");
+            return roundResults = "player";
+         }
+         else {
+            alert("Try again. Scissors cuts Paper.");
+            return roundResults = "cpu"
+         } 
+}
